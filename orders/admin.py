b054bc5ba32378483949b46b4d944bcbdeb16c2a
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-04-04 22:51:55 +0530

    Using webhooks to receive payment notifications -> Referencing Stripe payments in orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -6,6 +7,17 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ""
+
+
+order_payment.short_description = "Stripe payment"
 
 
 @admin.register(Order)
@@ -19,6 +31,7 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
+        order_payment,
         "created",
         "updated",
     ]
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -31,5 +31,7 @@ def stripe_webhook(request):
                 return HttpResponse(status=404)
             # mark order as paid
             order.paid = True
+            # store Stripe payment ID
+            order.stripe_id = session.payment_intent
             order.save()
     return HttpResponse(status=200)
