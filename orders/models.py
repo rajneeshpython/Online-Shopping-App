966f7ecdfca300c8b103792a2a693ec48ef47ea2
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-04-05 21:53:57 +0530

    Creating a coupon system for Stripe Checkout

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,10 @@
+from decimal import Decimal
+
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+from coupons.models import Coupon
 
 
 class Order(models.Model):
@@ -13,6 +18,12 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(
+        Coupon, related_name="orders", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    discount = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
 
     class Meta:
         ordering = ["-created"]
@@ -24,7 +35,17 @@ class Order(models.Model):
         return f"Order {self.id}"
 
     def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
     def get_stripe_url(self):
         if not self.stripe_id:
